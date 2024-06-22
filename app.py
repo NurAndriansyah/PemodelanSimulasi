17d353d3dfe3535c4a8efdc6371f166d90b4ef82
NurAndriansyah <122023973+NurAndriansyah@users.noreply.github.com>
2024-06-22 11:54:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ logger = logging.getLogger()
 
 # Judul Halaman
 st.title('Prediksi Harga Saham')
-st.subheader('Kelompok: 1301213475 Andri, 1301213268 Rey Reno Alvaro Ikhsan, 1301210554 Rivan Fauzan, Fadli')
+st.subheader('Kelompok: 1301213475 Andri, 1301213268 Rey Reno Alvaro Ikhsan, 1301210554 Rivan Fauzan, 1301203533 Muhammad Fadli Ramadhan')
 
 # Parameter Input
 st.sidebar.header('Parameter Simulasi')
